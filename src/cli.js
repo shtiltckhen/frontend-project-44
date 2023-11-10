271373f5e929ee0fe70e0e5be0f7bafb88642ae4
Белина <shtiltckhen@gmail.com>
2023-11-11 00:39:09 +0500

    fix

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
+export default () => {
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`)
+  console.log(`Hello, ${name}!`);
 };
-
-export { greeting };
