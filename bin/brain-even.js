0b821550f3bcc4476a00a29271eb830f9cd37f3d
Белина <shtiltckhen@gmail.com>
2023-11-13 21:44:53 +0500

    upgrade brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import gameEvenOrOdd from '../src/games/gameEvenOrOdd.js';
-import app from '../src/index.js';
+import brainCalc from '../src/games/gameEvenOrOdd.js';
 
-app(gameEvenOrOdd);
+brainCalc();
--- src/games/gameEvenOrOdd.js
+++ src/games/gameEvenOrOdd.js
@@ -1,10 +1,17 @@
-const getRandomInt = () => Math.floor(Math.random() * 100);
+import app from '../index.js';
+import getRandomInt from '../getRandomInt.js';
+import isEven from '../isEven.js';
+
+const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const getComponents = () => {
+  const number = getRandomInt(100);
+  const question = number;
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
 
 export default () => {
-  const task = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const number = getRandomInt();
-  const question = number;
-  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
-
-  return [task, question, correctAnswer];
+  app(getComponents, task);
 };
